c8c522932490c8de7896f80338494b5da5e1037a
Amina02-KC <aminagibadullina64@gmail.com>
2025-02-27 01:04:33 +0300

    HTML_Практика

---
+++ PR_L2/script.js
@@ -0,0 +1,5 @@
+const image = document.querySelector(".image")
+image.addEventListener("click", () => { 
+    image.src="https://i.pinimg.com/originals/d9/f8/e0/d9f8e0c745fe28199208ccf91abc9752.jpg"
+})
+console.log(image)
